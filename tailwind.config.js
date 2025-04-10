b28959c691ffad0e2fafe4a0ff7481bbb6b39b7f
santoish <santosanthoshsk81@gmail.com>
2025-04-10 14:49:37 +0530

    Fonts Updated

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    content: [
+      "./index.html",
+      "./src/**/*.{html,js,ts,jsx,tsx}",
+    ],
+  };
